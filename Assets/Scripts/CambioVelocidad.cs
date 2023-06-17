fd7a610c3fc3e20b06300e8cfc9e27a720345dd3
alekate <alekate2003@hotmail.com>
2023-06-16 22:03:48 -0300

    Juego Terminado
    Prototipo para Trabajo Integrador de la materia PIAPC
    Alejo Perot - Irina Altamirano

--- Assets/Scripts/CambioVelocidad.cs
+++ Assets/Scripts/CambioVelocidad.cs
@@ -9,15 +9,6 @@ using UnityEngine.AI;
 public class CambioVelocidad : MonoBehaviour
 {
 
-  public enum Estado
-  {
-   Alerta,
-   Patrulla,
-   Persecucion
-  }
-
-  public Estado estadoActual = Estado.Persecucion;
-
 
   private MaquinaDeEstados maquinaDeEstados;
   private NavMesh navMesh;
@@ -25,24 +16,18 @@ public class CambioVelocidad : MonoBehaviour
   private EstadoPersecucion estadoPersecucion;
   private PlayerController playerController;
   private NavMeshAgent _agent;
-  private RaycastHit hit;
-  private float _time;
+
 
   private Color color; // Variable local para almacenar el color
 
-  // private State estadoActual;
-
-  // public float minSpeed = 0.5f;
-  // public float maxSpeed = 5f;
-  // public MeshRenderer cubeRenderer;
 
   public Color colorMax = Color.red;
   public Color colorMedia = Color.yellow;
   public Color colorMin = Color.green;
 
-  public float velMaxima = 10.0f;
-  public float velMedia = 5.0f;
-  public float velMinima = 2.0f;
+  public float velMaxima = 7f;
+  public float velMedia = 5f;
+  public float velMinima = 2f;
 
 
   void OnEnable()
@@ -52,7 +37,7 @@ public class CambioVelocidad : MonoBehaviour
     controladorVision = GetComponent<ControladorVision>();
     estadoPersecucion = GetComponent<EstadoPersecucion>();
     playerController = GameObject.Find("Player").GetComponent<PlayerController>();
-    _agent.speed = 10f;
+    
 
   }
 
@@ -60,7 +45,7 @@ public class CambioVelocidad : MonoBehaviour
 
   void Start()
   {
-    // Asegurarse de que el objeto tiene un componente MeshRenderer
+    //Asegurarse de que el objeto tiene un componente MeshRenderer
     meshRenderer = GetComponent<MeshRenderer>();
     if (meshRenderer == null)
     {
@@ -73,52 +58,27 @@ public class CambioVelocidad : MonoBehaviour
   {
 
     float velocidadActual = _agent.velocity.magnitude; // Variable local para almacenar la velocidad actual
-
-    if (estadoActual == Estado.Persecucion)
-    {
-      // Calcula la velocidad actual del agente
-      velocidadActual = _agent.velocity.magnitude;
+    
+    // Calcula la velocidad actual del agente
+    velocidadActual = _agent.velocity.magnitude;
 
       
-      if (velocidadActual >= velMaxima)
-      {
-        color = colorMax;
-      }
-      else if (velocidadActual >= velMedia)
-      {
-        color = colorMedia;
-      }
-      else
-      {
-        color = colorMin;
-      }
-
-      meshRenderer.material.color = color;
+    if (velocidadActual >= velMaxima)
+    {
+      color = colorMax;
+    }
+    else if (velocidadActual >= velMedia)
+    {
+      color = colorMedia;
+    }
+    else
+    {
+      color = colorMin;
     }
 
-
-
+    meshRenderer.material.color = color;
     
-    //if (estadoActual == Estado.Persecucion)
-    //{
-      //Color color;
 
-      //if (velocidadActual >= velMaxima)
-      //{
-        //color = colorMax;
-      //}
-      //else if (velocidadActual >= velMedia)
-      //{
-        //color = colorMedia;
-      //}
-      //else
-      //{
-        //color = colorMin;
-      //}
-
-         meshRenderer.material.color = color;
-
-    //}
   }
 
 
